3d46b5ac14569ca8a3f36052025582643c3f5997
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2024-11-06 18:26:50 +0500

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
 import App from "next/app";
 import Hero from '@/components/Hero'
-
+import Gsap from '@/components/Gsap'
 export default function Home() {
   return (
     <>
+
   <Hero/>
+  <Gsap/>
     </>
   );
 }
